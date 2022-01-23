a892d6e7472aa529642977f9f4d44193f2d39b38
AlpHerk <secra@qq.com>
2022-01-23 12:41:24 +0800

    v6.2.0

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,6 @@
 from sys import path
 path.append(r"lib")
+
+from constants import *
+from update import *
+from utils import *
--- wdo/__init__.py
+++ wdo/__init__.py
@@ -1,2 +1,8 @@
 from sys import path
 path.append(r"wdo")
+
+# 导入UI依赖的资源文件
+import resource_rc
+
+# 导入Windows界面类
+from wdo.WdoAboutLogin import *
