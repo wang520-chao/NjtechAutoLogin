e28cb67961c3406a7bc93b0fb25a09ec1646dc05
AlpHerk <secra@qq.com>
2022-02-22 20:55:10 +0800

    v6.2.0

--- windo/__init__.py
+++ windo/__init__.py
@@ -1,7 +1,3 @@
 from sys import path
 from os.path import dirname
 path.append(dirname(__file__))
-
-# 导入Windows界面类
-# from win_login import WinLogin, WinLoginControl
-# from win_adh_login import WinFeedback, WinGiveReward, WinUpdateDialog
